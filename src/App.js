5c974f2eeb71d0c55979cf72169a5d2c2e842bb0
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-04-02 14:11:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default function App() {
   }
   const routers = createBrowserRouter([
     {
-      path: "/freshcart",
+      path: "/FreshCart",
       element: <Layout UserData={UserData} logOut={logOut} />,
       children: [
         { path: "/freshcart", element: <Home setisLoading={setisLoading} /> },
