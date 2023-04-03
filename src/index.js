4b516ae97bf97384b1e3ef97e72bf198dac9753d
elsharkawy159 <111247064+elsharkawy159@users.noreply.github.com>
2023-04-03 04:53:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode basename="/FreshCart">
     <App />
   </React.StrictMode>
 );
